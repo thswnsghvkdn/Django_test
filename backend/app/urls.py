86cf72d8413d4bf0babe146076915fb1995ffec1
xxun_jh <ths3630802@naver.com>
2021-10-16 20:17:59 +0900

    업데이트 딜리트 적용

---
+++ backend/app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('join/', views.join , name="join"), #name join 은 그냥 안해도 됨
+    path('login/', views.login , name="login"), #name join 은 그냥 안해도 됨
+    path('logout/', views.logout , name="logout"), #name join 은 그냥 안해도 됨
+    path('write/', views.write , name="write"), #name join 은 그냥 안해도 됨
+    path('update/<int:pk>', views.update , name="update"), #name join 은 그냥 안해도 됨
+    path('delete/<int:pk>', views.delete , name="delete"), #name join 은 그냥 안해도 됨
+    path('', views.home, name="home")
+]
